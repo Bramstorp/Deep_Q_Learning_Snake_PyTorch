d9d21012e35f0c6407bccfea87fd18aaa13d4279
Mathias Bramstorp <m.bramstorp@stofanet.dk>
2021-05-14 18:19:02 +0200

    added ai training function

--- agent.py
+++ agent.py
@@ -12,7 +12,10 @@ LEARNING_RATE = 0.001
 
 class Agent:
     def __init__(self):
-        pass
+        self.number_games = 0
+        self.epsilon = 0
+        self.gamma = 0
+        self.memory = deque(maxlen=MAX_MEMORY)
 
     def get_state(self, game):
         pass
@@ -23,9 +26,53 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, game_over):
         pass
 
     def get_action(self, state):
         pass
 
+
+def train():
+    plot_scores = []
+    plot_avg_scores = []
+    total_score = []
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+
+    while True:
+        # get old state
+        old_state = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(old_state)
+
+        # perform move and get a new state
+        reward, game_over, score = game.play_step(final_move)
+        new_state = agent.get_state(game)
+
+        # train short memory
+        agent.train_long_memory(old_state, final_move,
+                                reward, new_state, game_over)
+
+        # remember
+        agent.remember(old_state, final_move,
+                       reward, new_state, game_over)
+
+        if game_over:
+            # train long memory and plot result
+            game.reset()
+            agent.number_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+
+            print("Game:", agent.number_games)
+            print("Score:", score)
+            print("Record:", record)
+
+
+if __name__ == '__main__':
+    train()
